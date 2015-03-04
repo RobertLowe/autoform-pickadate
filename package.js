93597996ec2cf3f0162f71e8d5d9bfa885195d8c
Robert Lowe <robert@iblargz.com>
2015-03-04 18:47:21 -0500

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "robertlowe:autoform-pickadate",
-  version: "0.1.0",
+  version: "0.1.1",
   summary: "pickadate.js for autoform",
   git: "https://github.com/robertlowe/autoform-pickadate"
 });
